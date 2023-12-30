09edb323f0662e2baacc9af9e21ff88fd8b99197
PolinaPl <ppluhina0@gmail.com>
2023-12-30 15:51:28 +0700

    Домашнее задание Классы и объекты

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+Author pushkin = new Author("Александр", "Пушкин");
+Author lermontov = new Author("Михаил", "Лермонтов");
+
+Book kapitanskayaDochka = new Book("Капитанская дочка", pushkin, 1980);
+Book maskarad = new Book("Маскарад", lermontov, 1987);
+ System.out.println(kapitanskayaDochka.getTitle()+ " "+ kapitanskayaDochka.getAuthor().getFirstName()
+        + " " + kapitanskayaDochka.getAuthor().getSecondName() + " " + kapitanskayaDochka.getPublishYear());
+ System.out.println(maskarad.getTitle() + " "+ maskarad.getAuthor().getFirstName() + " " +
+                maskarad.getAuthor().getSecondName() + " " + maskarad.getPublishYear());
+
+kapitanskayaDochka.setPublishYear(2000);
+        System.out.println(kapitanskayaDochka.getTitle()+ " "+ kapitanskayaDochka.getAuthor().getFirstName()
+        + " " + kapitanskayaDochka.getAuthor().getSecondName() + " " + kapitanskayaDochka.getPublishYear());
+
+
     }
 }
