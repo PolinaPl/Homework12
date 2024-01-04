c3a257ae1530da8ff1dd212e51b3a4fbbf94cc5a
PolinaPl <ppluhina0@gmail.com>
2024-01-04 14:06:10 +0700

    Домашнее задание Методы объектов

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-Author pushkin = new Author("Александр", "Пушкин");
-Author lermontov = new Author("Михаил", "Лермонтов");
+        Author pushkin = new Author("Александр", "Пушкин");
+        Author lermontov = new Author("Михаил", "Лермонтов");
 
-Book kapitanskayaDochka = new Book("Капитанская дочка", pushkin, 1980);
-Book maskarad = new Book("Маскарад", lermontov, 1987);
- System.out.println(kapitanskayaDochka.getTitle()+ " "+ kapitanskayaDochka.getAuthor().getFirstName()
-        + " " + kapitanskayaDochka.getAuthor().getSecondName() + " " + kapitanskayaDochka.getPublishYear());
- System.out.println(maskarad.getTitle() + " "+ maskarad.getAuthor().getFirstName() + " " +
-                maskarad.getAuthor().getSecondName() + " " + maskarad.getPublishYear());
+        Book kapitanskayaDochka = new Book("Капитанская дочка", pushkin, 1980);
+        Book kapitanskayaDochka2 = new Book("Капитанская дочка", pushkin, 1980);
+        Book maskarad = new Book("Маскарад", lermontov, 1987);
+        System.out.println(kapitanskayaDochka);
+        System.out.println(maskarad);
+        System.out.println(kapitanskayaDochka.equals(maskarad));
+        System.out.println(kapitanskayaDochka.equals(kapitanskayaDochka2));
 
-kapitanskayaDochka.setPublishYear(2000);
-        System.out.println(kapitanskayaDochka.getTitle()+ " "+ kapitanskayaDochka.getAuthor().getFirstName()
-        + " " + kapitanskayaDochka.getAuthor().getSecondName() + " " + kapitanskayaDochka.getPublishYear());
+        kapitanskayaDochka.setPublishYear(2000);
 
-
+        System.out.println(kapitanskayaDochka.equals(kapitanskayaDochka2));
     }
 }
